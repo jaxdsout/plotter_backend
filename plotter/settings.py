fae50b360ea16a8bf64ea16752d3e9fd6542fade
jaxdsout <jaxon.southern@mac.com>
2024-09-11 17:27:38 -0500

    add another host for email

--- plotter/settings.py
+++ plotter/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = ['plotter-qxkl.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['plotter-qxkl.onrender.com', 'plotter-w3vt.onrender.com', 'localhost', '127.0.0.1']
 
 DEBUG = os.getenv('DEBUG')
 
