16380fc28e18c5dabe876f5804640fe687924c02
jaxdsout <jaxon.southern@mac.com>
2024-09-12 15:43:18 -0500

    slight serializer edit

--- user/serializers.py
+++ user/serializers.py
@@ -9,7 +9,7 @@ class UserCreateSerializer(UserCreateSerializer):
 
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'is_superuser', 'is_active', )
+        fields = ('id', 'first_name', 'last_name', 'email', 'password', 'is_superuser', 'is_active', )
 
 
 class UserLoadSerializer(UserSerializer):
