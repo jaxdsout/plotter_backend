2a426725100a9b04d14706ade47e90e3d093552b
jaxdsout <jaxon.southern@mac.com>
2024-09-20 12:50:05 -0500

    delete user view

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-
+from .views import UserDeleteView
 urlpatterns = [
     path('', include('djoser.urls')),
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('djoser.urls.jwt')),
-
+    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='custom_user_delete'),
 ]
 
---
+++ user/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserDeleteView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            user = User.objects.get(pk=kwargs['pk'])
+        except User.DoesNotExist:
+            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        if request.user.is_superuser:
+            user.delete()
+            return Response({"detail": "User deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+
+        return Response({"detail": "Permission denied."}, status=status.HTTP_403_FORBIDDEN)
