95c33b6abd781c7d7005ee44def78baa05695272
jaxdsout <jaxon.southern@mac.com>
2025-02-03 18:49:03 -0600

    password reset url update

--- plotter/settings.py
+++ plotter/settings.py
@@ -150,8 +150,8 @@ DJOSER = {
     'SEND_CONFIRMATION_EMAIL': True,
     'SET_USERNAME_RETYPE': True,
     'SET_PASSWORD_RETYPE': True,
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'reset-password/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'reset-email/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'verify/{user.id}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {
